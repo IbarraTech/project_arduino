e84886191bcf5b7c78c6022f1e8138fb8269d5ed
adrianaolave <adriana.olave@correounivalle.edu.co>
2023-10-30 21:51:04 -0500

    feat:Se agrega lineas de codiga al archivo lcd.h

--- src/modules/lcd/lcd.h
+++ src/modules/lcd/lcd.h
@@ -1,7 +1,16 @@
 #ifndef LCD_H
 #define LCD_H
 
-void config();
-void count();
+#include <Arduino.h>
+#include <LiquidCrystal.h>
+
+class pantalla_lcd {
+public:
+    pantalla_lcd(int rs, int en, int d4, int d5, int d6, int d7);
+    void iniciar();
+    void mostrar_distancia(float distancia);
+private:
+    LiquidCrystal _lcd;
+};
 
 #endif
